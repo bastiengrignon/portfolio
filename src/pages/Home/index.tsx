5591df42302e1c878201ff7df3c77ab2a2c0f3a7
bastiengrignon <basgrignon@gmail.com>
2021-01-17 19:58:41 +0100

    ADD SideBar implementation to Routes

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,11 @@
 import React from "react"
+import SideBar from "../../components/SideBar"
 
 const Home: React.FC = () => {
     return (
-        <div className="flex justify-center">
-            Home page
-            <i className="fab fa-github"/>
+        <div>
+            <SideBar/>
+            <div className="absolute top-0 ml-96">Home</div>
         </div>
     )
 }
---
+++ src/pages/Projects/index.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import SideBar from "../../components/SideBar"
+
+const Projects: React.FC = () => (
+    <div>
+        <SideBar/>
+        Project page
+    </div>
+)
+
+export default Projects
