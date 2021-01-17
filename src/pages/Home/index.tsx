f9ccb28779183d559b67dc87e713b7274303ed63
bastiengrignon <basgrignon@gmail.com>
2021-01-17 14:15:26 +0100

    ADD Home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Home: React.FC = () => {
+    return (
+        <div className="flex justify-center">
+            Home page
+        </div>
+    )
+}
+
+export default Home
