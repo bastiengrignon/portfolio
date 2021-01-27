9a8c01b1d9bf6815b2d112bbf983df99a02db7a3
bastiengrignon <basgrignon@gmail.com>
2021-01-28 00:53:38 +0100

    UPDATE design of CV

--- src/pages/CV/index.tsx
+++ src/pages/CV/index.tsx
@@ -3,12 +3,12 @@ import SideBar from "../../components/SideBar"
 
 const CV: React.FC = () => {
     return (
-        <div className="h-full w-max mt-4">
+        <div className="h-full w-full mt-4">
             <SideBar/>
-            <div className="pl-96 h-full w-full mx-auto">
+            <div className="pl-96 h-full w-screen">
                 <object width="1000" height="900"
                     data={process.env.PUBLIC_URL + "/doc/CV_Bastien_Grignon.pdf"}
-                    type="application/pdf" className="mx-24"/>
+                    type="application/pdf" className="mx-auto"/>
             </div>
         </div>
     )
