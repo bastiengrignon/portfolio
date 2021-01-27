bf0d2f323a861ee443a8acefe600c02d2a1d3465
bastiengrignon <basgrignon@gmail.com>
2021-01-28 00:05:31 +0100

    ADD Project component

---
+++ src/components/Project/index.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Project: React.FC = () => (
+    <li className="ml-5">
+        <div className="uppercase text-3xl">
+            Project Name
+        </div>
+        <div className="font-light text-xl">
+            Description of this project
+        </div>
+        <div className="text-base">
+
+        </div>
+    </li>
+)
+
+export default Project
