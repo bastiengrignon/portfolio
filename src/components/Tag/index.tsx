473c5d0eaedca318bf9d28fc9e9d1d78a3b62293
bastiengrignon <basgrignon@gmail.com>
2021-01-27 22:50:50 +0100

    ADD className to Tag component

--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -12,15 +12,16 @@ interface TagProps {
     text: string
     color?: TagColor
     icon?: ReactNode
+    className?: string
 }
 
-const Tag: React.FC<TagProps> = ({text, color = TagColor.blue, icon = <i className="fas fa-arrow-right"/>}) => {
+const Tag: React.FC<TagProps> = ({text, color = TagColor.blue, icon = <i className="fas fa-arrow-right"/>}, className = "") => {
     return (
         <div
-            className={`text-xs inline-flex items-baseline font-bold leading-sm uppercase px-3 py-1
+            className={`text-xs inline-flex items-baseline font-bold leading-sm uppercase px-3 py-1 mx-1
              bg-${color === TagColor.white ? color : `${color}-200`} 
              text-${color === TagColor.white ? "gray" : color}-700 
-             ${color === TagColor.white ? "border" : ""} rounded-full`}>
+             ${color === TagColor.white ? "border" : ""} rounded-full ${className}`}>
             {icon}
             <div className="ml-2">{text}</div>
         </div>
