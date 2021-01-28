8ebe2da2fcd22394f005374043ed411bc73bc5c3
bastiengrignon <basgrignon@gmail.com>
2021-01-28 01:13:48 +0100

    REMOVE guideline border

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -7,7 +7,7 @@ const Projects: React.FC = () => (
     <div className="inline-flex w-full">
         <LanguageSelector className="fixed top-0 right-0 z-10"/>
         <SideBar/>
-        <div className="h-full w-full pl-96 border-2 border-red-600">
+        <div className="h-full w-full pl-96">
             <ul className="list-none">
                 <Project/>
             </ul>
