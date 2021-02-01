0557969a099f5e5e575aeced7565afe62cc4fd65
bastiengrignon <basgrignon@gmail.com>
2021-02-01 01:26:51 +0100

    UPDATE jobs with tech list

--- src/constants/career.tsx
+++ src/constants/career.tsx
@@ -1,10 +1,10 @@
 import React, {ReactNode} from "react"
-import Tag from "../components/Tag"
 
 export interface CareerProps {
     date: string
     title: string
     description: string | ReactNode
+    techList?: string[]
 }
 
 const careerFolder = `${process.env.PUBLIC_URL}/img/career`
@@ -16,52 +16,32 @@ const career: CareerProps[] = [
         description: (
             <div className="flex flex-col mr-10 w-full">
                 <div className="inline-flex items-center text-justify justify-between w-full">
-                    <div>
-                        <p>
-                            Je suis actuellement en école d&apos;ingénieur en alternance à
-                            l&apos;ISEP dans le cursus <span className="font-bold">Ingénieur logiciel</span>.
-                            J&apos;ai pu réaliser différents projets pendant ces 3 années. En
-                            informatique et électronique, j&apos;ai pu effectuer des projets de site
-                            Web avec des technologies de bases (html/css/js/php) mais aussi en
-                            utilisant <span className="font-bold">React</span> aussi bien en
-                            <span className="italic"> Javascript</span> qu&apos;en <span
-                                className="italic">TypeScript</span>.
-                        </p>
-                        <div className="mt-5 font-semibold text-left">
-                            Technologies étudiées :
-                            {
-                                ["c/c++", "Java", "Spring", "html / css / js / php / mysql", "React", "aws", "git", "bash", "Typescript"].map((value, i) => (
-                                    <Tag key={i} text={value}/>
-                                ))
-                            }
-                        </div>
-                    </div>
+                    <p>
+                        Je suis actuellement en école d&apos;ingénieur en alternance à
+                        l&apos;ISEP dans le cursus <span
+                            className="font-bold">Ingénieur logiciel</span>.
+                        J&apos;ai pu réaliser différents projets pendant ces 3 années. En
+                        informatique et électronique, j&apos;ai pu effectuer des projets de site
+                        Web avec des technologies de bases (html/css/js/php) mais aussi en
+                        utilisant <span className="font-bold">React</span> aussi bien en
+                        <span className="italic"> Javascript</span> qu&apos;en <span
+                            className="italic">TypeScript</span>.
+                    </p>
                     <img src={`${careerFolder}/isep.jpg`} alt="Logo ISEP" className="w-32 ml-5"/>
                 </div>
-                <div className="inline-flex items-center text-justify justify-between">
-                    <div>
-                        <p className="mt-10">
-                            Je travaille chez Nokia en tant qu&apos;ingénieur logiciel sur le
-                            développement de la <span className="font-bold">5G</span>. Mon travail
-                            au sein de l&apos;équipe consiste à développer et implémenter de
-                            nouvelles
-                            fonctionnalités. Une de ces fonctionnalités pour lequel j&apos;ai pu
-                            aidé
-                            est l&apos;attache d&apos;un mobile à une antenne.
-                        </p>
-                        <div className="mt-5 font-semibold">
-                            Technologies étudiées :
-                            {
-                                ["c/c++", "bash", "git", "gerrit"].map((value, i) => (
-                                    <Tag key={i} text={value}/>
-                                ))
-                            }
-                        </div>
-                    </div>
+                <div className="inline-flex text-justify justify-between mt-4">
+                    <p>
+                        Je travaille chez Nokia en tant qu&apos;ingénieur logiciel sur le
+                        développement de la <span className="font-bold">5G</span>. Mon travail
+                        au sein de l&apos;équipe consiste à développer et implémenter de
+                        nouvelles fonctionnalités. Une de ces fonctionnalités pour lequel j&apos;ai
+                        pu aidé est l&apos;attache d&apos;un mobile à une antenne.
+                    </p>
                     <img src={`${careerFolder}/nokia.jpg`} alt="Logo Nokia" className="w-32 ml-5"/>
                 </div>
             </div>
-        )
+        ),
+        techList: ["c/c++", "Java", "Spring", "html / css / js / php / mysql", "React", "aws", "git", "bash", "Typescript"]
     },
     {
         date: "2018",
@@ -71,27 +51,15 @@ const career: CareerProps[] = [
                 <div>
                     <p>
                         Stage de 12 semaines dans le cadre de l&apos;IUT prolongé par un CDD pour
-                        l&apos;été
-                        en conception et développement informatique
+                        l&apos;été en conception et développement informatique
                     </p>
                     <p>
                         Durant ce stage j&apos;ai approfondi mes connaissances en développement
-                        C/C++,
-                        mais j&apos;ai aussi pu travailler sur un projet du début à la fin,
-                        c&apos;est à
-                        dire que j&apos;ai créer moi-même la carte électronique, j&apos;ai choisi
-                        les
-                        composants et eu l&apos;occasion de souder moi-même tous les composants sur
-                        cette carte. Ce stage a été très formateur
+                        C/C++, mais j&apos;ai aussi pu travailler sur un projet du début à la fin,
+                        c&apos;est à dire que j&apos;ai créer moi-même la carte électronique,
+                        j&apos;ai choisi les composants et eu l&apos;occasion de souder moi-même
+                        tous les composants sur cette carte. Ce stage a été très formateur
                     </p>
-                    <div className="mt-5 font-semibold">
-                        Technologies étudiées :
-                        {
-                            ["c/c++", "électronique", "LoRa", "Capteur à Ultrasons"].map((value, i) => (
-                                <Tag key={i} text={value}/>
-                            ))
-                        }
-                    </div>
                 </div>
                 <div>
                     <img src={`${careerFolder}/ines.png`} alt="Logo InEs"
@@ -100,7 +68,8 @@ const career: CareerProps[] = [
                         className="w-56 ml-5"/>
                 </div>
             </div>
-        )
+        ),
+        techList: ["c/c++", "électronique", "LoRa", "Capteur à Ultrasons"]
     },
     {
         date: "2016 - 2018",
@@ -116,19 +85,12 @@ const career: CareerProps[] = [
                         Tout au long de ce parcours, j&apos;ai pu apprendre beaucoup de choses dans
                         les domaines de l&apos;informatique ainsi que de l&apos;électronique
                     </p>
-                    <div className="mt-5 font-semibold">
-                        Technologies étudiées :
-                        {
-                            ["html", "css", "js", "c/c++", "php", "mysql", "java"].map((value, i) => (
-                                <Tag key={i} text={value} className="text-red-900"/>
-                            ))
-                        }
-                    </div>
                 </div>
                 <img src={`${careerFolder}/iut-angers.jpg`} alt="Logo IUT Angers"
                     className="w-48 ml-5"/>
             </div>
-        )
+        ),
+        techList: ["html", "css", "js", "c/c++", "php", "mysql", "java"]
     },
     {
         date: "2016 - 2018",
