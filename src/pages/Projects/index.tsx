e2a77c24dd03b43808e0c67e4cfbafd3948055c1
bastiengrignon <basgrignon@gmail.com>
2021-02-23 02:00:43 +0100

    ADD both github accounts (personal & educational)

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -9,10 +9,25 @@ const Projects: React.FC = () => (
         <LanguageSelector className="fixed top-0 right-0 z-10"/>
         <SideBar/>
         <div className="h-full w-full pl-96">
+            <p className="m-10 text-2xl font-medium">
+                Vous pouvez retrouver tous les projets personnels et publics auquel j&apos;ai pu
+                participer sur mon <a href="https://github.com/bastiengrignon"
+                    rel="noreferrer noopener" target="_blank"
+                    className="underline text-pink-600 hover:text-pink-800">GitHub</a>
+                <br/>
+                Ainsi que mes projets réalisés lors de mon cursus à l&apos;ISEP avec différentes
+                personnes regroupés sur <a href="https://github.com/ISEP-dev"
+                    rel="noreferrer noopener" target="_blank"
+                    className="underline text-pink-600 hover:text-pink-800">
+                ce GitHub
+                </a>
+
+            </p>
             <ul className="list-none">
                 {
                     projects.map((project, i) => (
-                        <Project key={i} name={project.name} description={project.description} img={project.img} tagList={project.tagList}/>
+                        <Project key={i} name={project.name} description={project.description}
+                            img={project.img} tagList={project.tagList}/>
                     ))
                 }
             </ul>
