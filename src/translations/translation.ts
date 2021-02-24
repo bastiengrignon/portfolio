3738bcee48791f3018d983a7e8b28ab5a00c12f8
bastiengrignon <basgrignon@gmail.com>
2021-02-24 14:44:48 +0100

    ADD career translations

--- src/translations/translation.ts
+++ src/translations/translation.ts
@@ -1,12 +1,16 @@
 import common_fr from "./fr/common.json"
+import career_fr from "./fr/career.json"
 import common_en from "./en/common.json"
+import carrer_en from "./en/career.json"
 
 export const fr = {
-    common: common_fr
+    common: common_fr,
+    career: career_fr
 }
 
 export const en = {
-    common: common_en
+    common: common_en,
+    career: carrer_en
 }
 
 const translations = { fr, en }
