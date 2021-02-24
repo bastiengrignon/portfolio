5c6bb3f90b58839bc4a24275fe5a03a434943ed6
bastiengrignon <basgrignon@gmail.com>
2021-02-24 14:46:19 +0100

    UPDATE Timeline to handle i18n translations

--- src/components/Timeline/index.tsx
+++ src/components/Timeline/index.tsx
@@ -1,16 +1,16 @@
 import React from "react"
-import {CareerProps} from "../../constants/career"
 import Tag from "../Tag"
 import {useTranslation} from "react-i18next"
+import {careerFolder, CareerProps} from "../../pages/Home"
 
 const Timeline: React.FC<CareerProps> = ({date, title, description, techList}) => {
     const {t} = useTranslation()
     return (
         <li className="inline-flex w-full py-6 pr-24">
             <div className="inline-flex items-start">
-                <div className="inline-flex items-center justify-between w-32">
-                    <div className="text-gray-500 font-light italic whitespace-nowrap">{date}</div>
-                    <span className="flex items-center h-4 w-4 z-10 mx-4">
+                <div className="inline-flex items-start justify-between w-32">
+                    <div className="text-gray-500 font-light italic flex-wrap w-20">{date}</div>
+                    <span className="h-4 w-4 z-10 mx-4">
                         <span
                             className="absolute rounded-full h-4 w-4 bg-blue-600 border-2 border-gray-200"/>
                         <span
@@ -23,7 +23,18 @@ const Timeline: React.FC<CareerProps> = ({date, title, description, techList}) =
                     {title}
                 </div>
                 <div className="ml-12 pb-4 pr-4">
-                    <div>{description}</div>
+                    <div className="inline-flex items-center text-justify justify-between mr-10 w-full">
+                        <div className="w-3/4">
+                            {description.text}
+                        </div>
+                        <div className="flex flex-col items-center w-1/4">
+                            {
+                                description.images.map((image, index) => (
+                                    <img key={index} src={`${careerFolder}/${image.src}`} alt={image.alt} className={`ml-5 ${image.className}`}/>
+                                ))
+                            }
+                        </div>
+                    </div>
                     {
                         techList && <div className="font-semibold mt-4 text-left">
                             {t("study_tech")}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,28 +1,146 @@
-import React from "react"
+import React, {ReactNode} from "react"
 import Timeline from "../../components/Timeline"
-import career from "../../constants/career"
+import {Trans, useTranslation} from "react-i18next"
 
-const Home: React.FC = () => (
-    <div className="inline-flex w-full">
-        <div className="pl-96 h-full w-full pt-10">
-            <p className="text-4xl uppercase text-gray-600 pb-5 px-5">Portfolio</p>
-            <ul className="list-none px-10 relative">
-                <div
-                    className="absolute border-r-2 border-gray-400 border-dotted h-full z-0 mt-7"
-                    style={{marginLeft: "6.45rem"}}/>
-                {
-                    career.map((job, i) => (
-                        <div key={i}>
-                            <Timeline date={job.date} title={job.title} description={job.description} techList={job.techList} />
-                            <div className="border border-gray-300 w-4/5 mx-auto my-2"/>
-                        </div>
+interface ImageCareer {
+    src: string
+    alt: string
+    className?: string
+}
 
-                    ))
-                }
-            </ul>
+interface DescriptionCareer {
+    text: string | ReactNode
+    images: ImageCareer[]
+}
 
+export interface CareerProps {
+    date: string
+    title: string
+    description: DescriptionCareer
+    techList?: string[]
+}
+
+export const careerFolder = `${process.env.PUBLIC_URL}/img/career`
+
+const Home: React.FC = () => {
+    const {t} = useTranslation("career")
+
+    const career: CareerProps[] = [
+        {
+            date: t("career.0.date"),
+            title: t("career.0.title"),
+            description: {
+                text: <Trans i18nKey={"career:career.0.description"}
+                    components={{
+                        italic: <i/>,
+                        bold: <strong/>,
+                        newline: <hr className="my-5"/>
+                    }}
+                />,
+                images: [
+                    {
+                        src: "isep.jpg",
+                        alt: "Logo ISEP",
+                        className: "w-32 mb-2"
+                    },
+                    {
+                        src: "nokia.jpg",
+                        alt: "Logo Nokia",
+                        className: "w-32"
+                    }
+                ]
+            },
+            techList: ["c/c++", "Java", "Spring", "html / css / js / php / mysql", "React", "aws", "git", "bash", "Typescript"]
+        },
+        {
+            date: t("career.1.date"),
+            title: t("career.1.title"),
+            description: {
+                text: <Trans i18nKey={"career:career.1.description"}
+                    components={{bold: <strong/>, newline: <br/>}}/>,
+                images: [
+                    {
+                        src: "ines.png",
+                        alt: "Logo InEs",
+                        className: "w-56 my-2"
+                    },
+                    {
+                        src: "sensor_case.jpg",
+                        alt: "Sensor Case",
+                        className: "w-56"
+                    }
+                ]
+            },
+            techList: ["c/c++", "électronique", "LoRa", "Capteur à Ultrasons"]
+        },
+        {
+            date: t("career.2.date"),
+            title: t("career.2.title"),
+            description: {
+                text: <Trans i18nKey={"career:career.2.description"}
+                    components={{newline: <br/>}}/>,
+                images: [
+                    {
+                        src: "iut-angers.jpg",
+                        alt: "Logo IUT Angers",
+                        className: "w-48"
+                    }
+                ]
+            },
+            techList: ["html", "css", "js", "c/c++", "php", "mysql", "java"]
+        },
+        {
+            date: t("career.3.date"),
+            title: t("career.3.title"),
+            description: {
+                text: <Trans i18nKey={"career:career.3.description"}
+                    components={{newline: <br/>}}/>,
+                images: [
+                    {
+                        src: "tennis.jpg",
+                        alt: "Tennis Club TCA"
+                    }
+                ]
+            }
+        },
+        {
+            date: t("career.4.date"),
+            title: t("career.4.title"),
+            description: {
+                text: t("career.4.description"),
+                images: [
+                    {
+                        src: "chevrollier.jpg",
+                        alt: "Lycee Chevrollier",
+                        className: "w-32 h-32"
+                    }
+                ]
+            }
+        }
+    ]
+    return (
+        <div className="inline-flex w-full">
+            <div className="pl-96 h-full w-full pt-10">
+                <p className="text-4xl uppercase text-gray-600 pb-5 px-5">Portfolio</p>
+                <ul className="list-none px-10 relative">
+                    <div
+                        className="absolute border-r-2 border-gray-400 border-dotted h-full z-0 mt-7"
+                        style={{marginLeft: "6.44rem"}}/>
+                    {
+                        career.map((job, i) => (
+                            <div key={i}>
+                                <Timeline date={job.date} title={job.title}
+                                    description={job.description} techList={job.techList}/>
+                                <div className="border border-gray-300 w-4/5 mx-auto my-2"/>
+                            </div>
+
+                        ))
+                    }
+                </ul>
+
+            </div>
         </div>
-    </div>
-)
+    )
+}
 
 export default Home
